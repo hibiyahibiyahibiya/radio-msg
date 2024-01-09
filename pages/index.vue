3e706657a95d5cf87ffcedd1475889dd1a967c4a
hibiyahibiyahibiya <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2024-01-09 07:59:09 +0000

    demo

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--
+  <div><input type="radio" style="accent-color: black" /><span>ある</span></div>
+  <div>
+    <label> <input type="radio" style="accent-color: black" />ない</label>
+  </div>
+  -->
+  <div>
+    <label> <input type="radio" style="accent-color: black" />ある</label>
+  </div>
+  <div>
+    <label> <input type="radio" style="accent-color: black" />どちらかというとある</label>
+  </div>
+  <div>
+    <label> <input type="radio" style="accent-color: black" />どちらとも言えない</label>
+  </div>
+  <div>
+    <label> <input type="radio" style="accent-color: black" />どちらかというとない</label>
+  </div>
+  <div>
+    <label> <input type="radio" style="accent-color: black" />ない</label>
+  </div>
+</template>
